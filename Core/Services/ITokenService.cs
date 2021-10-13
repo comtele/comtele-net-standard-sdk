d3de5cb79e0c47427822548a3bd16b748dcb0eef
Diego Vieira <divieira.silva@gmail.com>
2021-10-13 10:53:47 -0300

    adição de novos parâmetros nas rotinas de token

--- Core/Services/ITokenService.cs
+++ Core/Services/ITokenService.cs
@@ -8,13 +8,13 @@ namespace Comtele.Sdk.Core.Services
 {
     public interface ITokenService
     {
-        ServiceResult<object> SendToken(string phoneNumber, string prefix);
-        Task<ServiceResult<object>> SendTokenAsync(string phoneNumber, string prefix);
+        ServiceResult<object> SendToken(string phoneNumber, string prefix, bool enforceSecureValidation);
+        Task<ServiceResult<object>> SendTokenAsync(string phoneNumber, string prefix, bool enforceSecureValidation);
 
-        ServiceResult<object> SendToken(string phoneNumber);
-        Task<ServiceResult<object>> SendTokenAsync(string phoneNumber);
+        ServiceResult<object> SendToken(string phoneNumber, bool enforceSecureValidation);
+        Task<ServiceResult<object>> SendTokenAsync(string phoneNumber, bool enforceSecureValidation);
 
-        ServiceResult<object> ValidateToken(string tokenCode);
-        Task<ServiceResult<object>> ValidateTokenAsync(string tokenCode);
+        ServiceResult<object> ValidateToken(string tokenCode, string phoneNumber);
+        Task<ServiceResult<object>> ValidateTokenAsync(string tokenCode, string phoneNumber);
     }
 }
--- Services/TokenService.cs
+++ Services/TokenService.cs
@@ -12,7 +12,7 @@ namespace Comtele.Sdk.Services
     {
         public TokenService(string apiKey) : base(apiKey) { }
 
-        public ServiceResult<object> SendToken(string phoneNumber, string prefix)
+        public ServiceResult<object> SendToken(string phoneNumber, string prefix, bool enforceSecureValidation)
         {
             var restClient = new RestClient(ENDPOINT_ADDRESS);
             var restRequest = new RestRequest("tokenmanager", Method.POST);
@@ -25,37 +25,37 @@ namespace Comtele.Sdk.Services
             return restResponse.Data;
         }
 
-        public ServiceResult<object> SendToken(string phoneNumber)
+        public ServiceResult<object> SendToken(string phoneNumber, bool enforceSecureValidation)
         {
-            return SendToken(phoneNumber, string.Empty);
+            return SendToken(phoneNumber, string.Empty, enforceSecureValidation);
         }
 
-        public async Task<ServiceResult<object>> SendTokenAsync(string phoneNumber, string prefix)
+        public async Task<ServiceResult<object>> SendTokenAsync(string phoneNumber, string prefix, bool enforceSecureValidation)
         {
-            return await Task.Run(() => SendToken(phoneNumber, prefix));
+            return await Task.Run(() => SendToken(phoneNumber, prefix, enforceSecureValidation));
         }
 
-        public async Task<ServiceResult<object>> SendTokenAsync(string phoneNumber)
+        public async Task<ServiceResult<object>> SendTokenAsync(string phoneNumber, bool enforceSecureValidation)
         {
-            return await Task.Run(() => SendToken(phoneNumber));
+            return await Task.Run(() => SendToken(phoneNumber, enforceSecureValidation));
         }
 
-        public ServiceResult<object> ValidateToken(string tokenCode)
+        public ServiceResult<object> ValidateToken(string tokenCode, string phoneNumber)
         {
             var restClient = new RestClient(ENDPOINT_ADDRESS);
             var restRequest = new RestRequest("tokenmanager", Method.PUT);
 
             restRequest.AddHeader("auth-key", ApiKey);
-            restRequest.AddJsonBody(new { tokenCode });
+            restRequest.AddJsonBody(new { tokenCode, phoneNumber });
 
             var restResponse = restClient.Execute<ServiceResult<object>>(restRequest);
 
             return restResponse.Data;
         }
 
-        public async Task<ServiceResult<object>> ValidateTokenAsync(string tokenCode)
+        public async Task<ServiceResult<object>> ValidateTokenAsync(string tokenCode, string phoneNumber)
         {
-            return await Task.Run(() => ValidateToken(tokenCode));
+            return await Task.Run(() => ValidateToken(tokenCode, phoneNumber));
         }
     }
 }
